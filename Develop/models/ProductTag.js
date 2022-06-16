5ceea6008e1be9fc5054b3071df3a4dd23a6f139
Jennifer Schmiett <bailliejm@gmail.com>
2022-06-15 19:41:57 -0500

    worked on code in the model directory

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const { all } = require('../routes/api/category-routes.js');
+const Category = require('./Category');
+const Product = require('./Product.js');
+const Tag = require('./Tag');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the Product model's id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the Tag model's id
+    }
   },
   {
     sequelize,
--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { all } = require('../routes/api/category-routes.js');
+const Product = require('./Product');
+const Category = require('./Category');
+const ProductTag = require('./ProductTag');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
